d16fd4716cf8e31ab411090270df717a3a96dcf4
goceanlab <aeekdw@gmail.com>
2019-12-05 12:41:51 +0900

    Updates

---
+++ precache-manifest.075c19182a65229152a5ae1aaa7d52bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6780ddf212285d7e206c362fa585b5f8",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "58fdf73e5013e2440742",
+    "url": "/movie_app_2019/static/css/main.ce262de1.chunk.css"
+  },
+  {
+    "revision": "c6751f57a4c51b316803",
+    "url": "/movie_app_2019/static/js/2.115b4f6b.chunk.js"
+  },
+  {
+    "revision": "58fdf73e5013e2440742",
+    "url": "/movie_app_2019/static/js/main.212e7cf4.chunk.js"
+  },
+  {
+    "revision": "de5e2538ac5f7139f893",
+    "url": "/movie_app_2019/static/js/runtime-main.d2d995b1.js"
+  }
+]);
