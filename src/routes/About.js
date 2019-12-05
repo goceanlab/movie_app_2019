b52035ff9d3f0d743913e613b9e1bdd147c32e5f
goceanlab <aeekdw@gmail.com>
2019-12-05 15:45:21 +0900

    Final

---
+++ src/routes/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="container">
+      <div className="container__about">
+        <span className="about">I like these movies.</span>
+      </div>
+    </div>
+  );
+}
+
+export default About;
